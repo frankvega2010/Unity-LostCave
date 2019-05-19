6b9c9becae62b48051cba7d2824071e15c980a40
Franco <franco_vega@msn.com>
2019-05-19 02:33:40 -0300

    Added trapdoor location & fixed player surroundings

--- Lost Cave/Assets/Scripts/Gameplay/RandomWallLocation.cs
+++ Lost Cave/Assets/Scripts/Gameplay/RandomWallLocation.cs
@@ -5,9 +5,12 @@ using UnityEngine;
 public class RandomWallLocation : MonoBehaviour
 {
     public GameObject player;
+    public GameObject trapDoor;
     public GameObject breakableWall;
     public int maxWalls;
     public List<Vector2> usedPositions;
+    public List<Vector2> reservedPositions;
+    public Vector2 trapDoorLocation;
 
     private bool isRepeated;
 
@@ -18,21 +21,21 @@ public class RandomWallLocation : MonoBehaviour
         {
             maxWalls = 120;
         }
+        usedPositions.Add(new Vector2(0, 0));
+        reservedPositions.Add(new Vector2(player.transform.position.x, player.transform.position.z));
 
-        usedPositions.Add(new Vector2(player.transform.position.x, player.transform.position.z));
+        reservedPositions.Add(new Vector2(player.transform.position.x + 5, player.transform.position.z));
+        reservedPositions.Add(new Vector2(player.transform.position.x - 5, player.transform.position.z));
 
-        usedPositions.Add(new Vector2(player.transform.position.x + 5, player.transform.position.z));
-        usedPositions.Add(new Vector2(player.transform.position.x - 5, player.transform.position.z));
+        reservedPositions.Add(new Vector2(player.transform.position.x, player.transform.position.z + 5));
+        reservedPositions.Add(new Vector2(player.transform.position.x, player.transform.position.z - 5));
 
-        usedPositions.Add(new Vector2(player.transform.position.x, player.transform.position.z + 5));
-        usedPositions.Add(new Vector2(player.transform.position.x, player.transform.position.z - 5));
+        reservedPositions.Add(new Vector2(player.transform.position.x - 5, player.transform.position.z + 5));
+        reservedPositions.Add(new Vector2(player.transform.position.x + 5, player.transform.position.z + 5));
 
-        usedPositions.Add(new Vector2(player.transform.position.x - 5, player.transform.position.z + 5));
-        usedPositions.Add(new Vector2(player.transform.position.x + 5, player.transform.position.z + 5));
-
-        usedPositions.Add(new Vector2(player.transform.position.x - 5, player.transform.position.z - 5));
-        usedPositions.Add(new Vector2(player.transform.position.x + 5, player.transform.position.z - 5));
-
+        reservedPositions.Add(new Vector2(player.transform.position.x - 5, player.transform.position.z - 5));
+        reservedPositions.Add(new Vector2(player.transform.position.x + 5, player.transform.position.z - 5));
+                    
         for (int i = 0; i < maxWalls; i++)
         {
             isRepeated = false;
@@ -66,11 +69,34 @@ public class RandomWallLocation : MonoBehaviour
             }
             else
             {
-                usedPositions.Add(new Vector2(randomLocationIndexX, randomLocationIndexZ));
+                
                 GameObject instanciatedWall = Instantiate(breakableWall, new Vector3(randomLocationIndexX * 5, 3, randomLocationIndexZ * 5), breakableWall.transform.rotation);
                 instanciatedWall.SetActive(true);
+
+                for (int b = 0; b < reservedPositions.Count; b++)
+                {
+                    if(instanciatedWall.transform.position.x == reservedPositions[b].x && instanciatedWall.transform.position.z == reservedPositions[b].y)
+                    {
+                        i--;
+                        Destroy(instanciatedWall);
+                        isRepeated = true;
+                    }
+                }
+
+                if(!isRepeated)
+                {
+                    usedPositions.Add(new Vector2(randomLocationIndexX, randomLocationIndexZ));
+
+                    if (i == maxWalls - 1)
+                    {
+                        trapDoorLocation.x = instanciatedWall.transform.position.x;
+                        trapDoorLocation.y = instanciatedWall.transform.position.z;
+                    }
+                }
             }
-            
+           
         }
+
+        trapDoor.transform.position = new Vector3(trapDoorLocation.x, trapDoor.transform.position.y, trapDoorLocation.y);
     }
 }
