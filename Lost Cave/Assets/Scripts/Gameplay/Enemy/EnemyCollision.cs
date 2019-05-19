4aa0ce4295f91452cd9420db528ff544fdc20274
Franco <franco_vega@msn.com>
2019-05-19 00:27:45 -0300

    Added enemy movement & collision

---
+++ Lost Cave/Assets/Scripts/Gameplay/Enemy/EnemyCollision.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyCollision : MonoBehaviour
+{
+
+    private EnemyMovement enemyMovement;
+    //private EnemyMovement.dir currentDir;
+    private void Start()
+    {
+        enemyMovement = GetComponent<EnemyMovement>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        //currentDir = enemyMovement.enemyDir;
+
+        switch (collision.gameObject.tag)
+        {
+            case "wall":
+                checkDirection();
+                break;
+            case "breakableWall":
+                checkDirection();
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void checkDirection()
+    {
+        switch (enemyMovement.enemyDir)
+        {
+            case EnemyMovement.dir.moveUp:
+                enemyMovement.enemyDir = EnemyMovement.dir.moveDown;
+                break;
+            case EnemyMovement.dir.moveDown:
+                enemyMovement.enemyDir = EnemyMovement.dir.moveUp;
+                break;
+            case EnemyMovement.dir.moveLeft:
+                enemyMovement.enemyDir = EnemyMovement.dir.moveRight;
+                break;
+            case EnemyMovement.dir.moveRight:
+                enemyMovement.enemyDir = EnemyMovement.dir.moveLeft;
+                break;
+            default:
+                break;
+        }
+    }
+}
---
+++ Lost Cave/Assets/Scripts/Gameplay/Enemy/EnemyMovement.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    public enum dir
+    {
+        moveUp,
+        moveDown,
+        moveLeft,
+        moveRight,
+        maxMoves
+    }
+
+    public int speed;
+    public dir enemyDir;
+
+    // Start is called before the first frame update
+    //void Start()
+    //{
+        
+    //}
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        switch (enemyDir)
+        {
+            case dir.moveUp:
+                transform.position = transform.position + transform.forward * speed * Time.deltaTime;
+                break;
+            case dir.moveDown:
+                transform.position = transform.position + transform.forward * -speed * Time.deltaTime;
+                break;
+            case dir.moveLeft:
+                transform.position = transform.position + transform.right * -speed * Time.deltaTime;
+                break;
+            case dir.moveRight:
+                transform.position = transform.position + transform.right * speed * Time.deltaTime;
+                break;
+            default:
+                break;
+        }
+    }
+}
